a58a95791a5930da453a0e99557c35cb93c456c8
Mukul Malhotra <smilemukul2005@gmail.com>
2024-06-18 20:41:14 +0530

    update nodejs project

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Welcome to DevOps Security Lab" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello Students" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
